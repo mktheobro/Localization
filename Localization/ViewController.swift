fbdbf68793309a424cab77d5414258da986e5e11
mktheobro <theobrokuradoru@yahoo.co.jp>
2021-06-15 14:09:00 +0900

    Initial Commit

---
+++ Localization/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Localization
+//
+//  Created by 久保政人 on 2021/06/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
