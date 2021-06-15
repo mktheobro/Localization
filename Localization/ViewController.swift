a739dcd60e7bed866ed1f71a8661b089a18c90e0
mktheobro <theobrokuradoru@yahoo.co.jp>
2021-06-15 14:14:00 +0900

    first commit

--- Localization/ViewController.swift
+++ Localization/ViewController.swift
@@ -9,6 +9,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var label: UILabel!
+    @IBAction func sayHello(_ sender: Any) {
+        label.text = "Hello"
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
